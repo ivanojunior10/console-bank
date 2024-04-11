d6968ab93a57ab08b67c598fe8c0668b1a11c885
Ivano Alves <ivanojunior10@gmail.com>
2024-04-11 12:29:46 -0300

    u

---
+++ src/Desafio.java
@@ -0,0 +1,58 @@
+import java.util.Scanner;
+
+public class Desafio {
+    public static void main(String[] args) {
+        String nome = "Steve Rogers";
+        String tipoConta = "Corrente";
+        double saldo = 1599.99;
+        int opcao = 0;
+
+        System.out.println("*************************************");
+        System.out.println("\nNome do Cliente: " + nome);
+        System.out.println("Tipo conta: " + tipoConta);
+        System.out.println("Saldo atual: " + saldo);
+        System.out.println("*************************************");
+
+
+        String menu = """
+                ** Digite sua opção 
+                1 - Consultar saldo
+                2 - Transferir valor
+                3 - Receber valor
+                4 - Sair
+                
+                
+             
+                """;
+
+        Scanner leitura = new Scanner(System.in);
+
+        while (opcao != 4) {
+            System.out.println(menu);
+            opcao = leitura.nextInt();
+
+
+            if (opcao == 1){
+                System.out.println("O Saldo atualizadfo é " + saldo);
+            } else if (opcao == 2) {
+                System.out.println("Qual o valor que deseja transferir?");
+                double valor = leitura.nextDouble();
+                if (valor > saldo ){
+                    System.out.println("Saldo insuficiente");
+                } else {
+                    saldo -= valor;
+                    System.out.println("Novo saldo " + saldo);
+                }
+
+            } else if (opcao ==3){
+                System.out.println("Valor recebido: ");
+                double valor = leitura.nextDouble();
+                saldo += valor;
+                System.out.println("Movo saldo " + saldo);
+            } else if (opcao != 4){
+                System.out.println("Opção invalida");
+            }
+        }
+
+    }
+}
